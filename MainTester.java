943a6c7c0a57102e2b156a8d13ab0d814002e4c5
jdweak <jonathanweakliem@gmail.com>
2022-02-01 20:14:04 -0800

    add tester

---
+++ MainTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTester {
+    
+    @Test
+    public void testSubtract(){
+        assertEquals(0, Main.subtract(4, 3));
+    }
+}
